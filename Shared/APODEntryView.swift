810aebfa190d831b779c4391b3acacd2b453e552
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2020-09-25 12:06:30 -0600

    Show date in upper right; change preview data

--- Shared/APODEntryView.swift
+++ Shared/APODEntryView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 import struct WidgetKit.WidgetPreviewContext
 
+extension DateFormatter {
+  static let monthDay = with(DateFormatter()) {
+    $0.setLocalizedDateFormatFromTemplate("MMM dd")
+  }
+}
+
 struct PhotoView: View {
+  let date: Date
   let image: AnyView?
   let caption: String?
   let copyright: String?
 
   var body: some View {
-    ZStack() {
+    ZStack {
       Color.black.edgesIgnoringSafeArea(.all)
         .ifLet(image) {
           $0.overlay($1)
         }
 
       HStack {
-        VStack(alignment: .leading) {
-          //          Text(entry.date.asDate()!, style: .date).font(.caption2)
+        VStack(alignment: .leading, spacing: 4) {
+          HStack {
+            Spacer()
+            Text(date, formatter: DateFormatter.monthDay)
+              .font(.caption2)
+          }
           Spacer()
           if let caption = caption {
-            Text(caption).font(.system(.footnote)).bold()
+            Text(caption).font(.system(.footnote)).bold().lineSpacing(-4)
           }
           if let copyright = copyright {
             Text(copyright).font(.system(.caption2))
@@ -33,8 +44,7 @@ struct PhotoView: View {
         }
         Spacer()
       }
-      .padding(.horizontal, 16)
-      .padding(.vertical, 12)
+      .padding(EdgeInsets(top: 14, leading: 16, bottom: 12, trailing: 10))
       .foregroundColor(Color(.sRGB, white: 0.9))
       .shadow(color: .black, radius: 2, x: 0.0, y: 0.0)
     }
@@ -58,16 +68,20 @@ public struct APODEntryView: View {
       Group {}
 
     case .loading:
-      ProgressView()
+      Color.black
+        .overlay(ProgressView())
+        .colorScheme(.dark)
 
     case .loaded(.failure(let error)):
       Group {}
 
     case .loaded(.success(let entry)):
       if let image = entry.loadImage() {
-        PhotoView(image: AnyView(Image(uiImage: image).resizable().aspectRatio(contentMode: .fill)),
-                  caption: entry.title,
-                  copyright: entry.copyright)
+        PhotoView(
+          date: entry.date.asDate()!,
+          image: AnyView(Image(uiImage: image).resizable().aspectRatio(contentMode: .fill)),
+          caption: entry.title,
+          copyright: entry.copyright)
       } else {
         ZStack {
           Color.black.edgesIgnoringSafeArea(.all)
@@ -86,17 +100,23 @@ public struct APODEntryView: View {
 struct APODEntryView_Previews: PreviewProvider {
   static var previews: some View {
     let previewJSON = """
-  {
-  "copyright": "Luca Vanzella",
-  "date": "2020-09-22",
-  "explanation": "Does the Sun set in the same direction every day? No, the direction of sunset depends on the time of the year. Although the Sun always sets approximately toward the west, on an equinox like today the Sun sets directly toward the west. After today's September equinox, the Sun will set increasingly toward the southwest, reaching its maximum displacement at the December solstice.  Before today's September equinox, the Sun had set toward the northwest, reaching its maximum displacement at the June solstice. The featured time-lapse image shows seven bands of the Sun setting one day each month from 2019 December through 2020 June.  These image sequences were taken from Alberta, Canada -- well north of the Earth's equator -- and feature the city of Edmonton in the foreground.  The middle band shows the Sun setting during the last equinox -- in March.  From this location, the Sun will set along this same equinox band again today.",
-  "hdurl": "https://apod.nasa.gov/apod/image/2009/SunsetMonths_Vanzella_2400.jpg",
+{
+  "copyright": "Adam Block",
+  "date": "2020-09-25",
+  "explanation": "The Great Spiral Galaxy in Andromeda (also known as M31), a mere 2.5 million light-years distant, is the closest large spiral to our own Milky Way. Andromeda is visible to the unaided eye as a small, faint, fuzzy patch, but because its surface brightness is so low, casual skygazers can't appreciate the galaxy's impressive extent in planet Earth's sky. This entertaining composite image compares the angular size of the nearby galaxy to a brighter, more familiar celestial sight. In it, a deep exposure of Andromeda, tracing beautiful blue star clusters in spiral arms far beyond the bright yellow core, is combined with a typical view of a nearly full Moon. Shown at the same angular scale, the Moon covers about 1/2 degree on the sky, while the galaxy is clearly several times that size. The deep Andromeda exposure also includes two bright satellite galaxies, M32 and M110 (below and right).",
+  "hdurl": "https://apod.nasa.gov/apod/image/2009/m31abtpmoon.jpg",
   "media_type": "image",
   "service_version": "v1",
-  "title": "Equinox in the Sky",
-  "url": "https://apod.nasa.gov/apod/image/2009/SunsetMonths_Vanzella_1080_annotated.jpg"
-  }
-  """.data(using: .utf8)!
+  "title": "Moon over Andromeda",
+  "url": "https://apod.nasa.gov/apod/image/2009/m31abtpmoon1024.jpg"
+}
+""".data(using: .utf8)!
+
+    APODEntryView(
+      entry: with(try! JSONDecoder().decode(APODEntry.self, from: previewJSON)) {
+        $0.PREVIEW_overrideImage = #imageLiteral(resourceName: "sampleImage")
+      })
+      .previewContext(WidgetPreviewContext(family: .systemSmall))
 
     APODEntryView(
       entry: with(try! JSONDecoder().decode(APODEntry.self, from: previewJSON)) {
@@ -104,10 +124,10 @@ struct APODEntryView_Previews: PreviewProvider {
       })
       .previewContext(WidgetPreviewContext(family: .systemMedium))
 
-    PhotoView(image: AnyView(Image(uiImage: #imageLiteral(resourceName: "sampleImage")).resizable().aspectRatio(3, contentMode: .fill)), caption: "Hello", copyright: "There")
+    PhotoView(date: Date(), image: AnyView(Image(uiImage: #imageLiteral(resourceName: "sampleImage")).resizable().aspectRatio(3, contentMode: .fill)), caption: "Hello", copyright: "There")
       .previewContext(WidgetPreviewContext(family: .systemMedium))
 
-    PhotoView(image: AnyView(Image(uiImage: #imageLiteral(resourceName: "sampleImage")).resizable().aspectRatio(0.3, contentMode: .fill)), caption: "Hello", copyright: "There")
+    PhotoView(date: Date(), image: AnyView(Image(uiImage: #imageLiteral(resourceName: "sampleImage")).resizable().aspectRatio(0.3, contentMode: .fill)), caption: "Hello", copyright: "There")
       .previewContext(WidgetPreviewContext(family: .systemMedium))
 
     APODEntryView(
